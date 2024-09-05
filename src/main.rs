f1ab8019fe72301828e24eb786d6fb358c712422
Szabó István <pisti4395@gmail.com>
2024-09-05 20:19:20 +0200

    pass 1st stage

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,17 @@ use std::io::{self, Write};
 
 fn main() {
     // Uncomment this block to pass the first stage
-    print!("$ ");
-    io::stdout().flush().unwrap();
-    //
-    // Wait for user input
-    let stdin = io::stdin();
-    let mut input = String::new();
-    stdin.read_line(&mut input).unwrap();
-    run_command(input);
+
+    loop{
+        print!("$ ");
+        io::stdout().flush().unwrap();
+        //
+        // Wait for user input
+        let stdin = io::stdin();
+        let mut input = String::new();
+        stdin.read_line(&mut input).unwrap();
+        run_command(input);
+    }
 }
 
 fn run_command(command:String) {
