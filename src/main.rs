1d1a79f0cb866a8984218ab59151883b777eec99
Szabó István <pisti4395@gmail.com>
2025-03-17 21:27:41 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,25 @@
 use std::io::{self, Write};
+use std::collections::HashMap;
 
 fn main()  {
+	let mut builtins: HashMap<String, Box<dyn Fn(String)-> i32>> = HashMap::new();
+	builtins.insert(
+		"echo".to_string(),
+		Box::new(
+			move |mut input: String|{
+				println!("{}", input.split_off(5));
+				return 0;
+			}
+		)
+	);
+	builtins.insert(
+		"exit".to_string(),
+		Box::new(
+			move |_input: String|{
+				return 1;
+			}
+		)
+	);
     loop{
         print!("$ ");
         io::stdout().flush().unwrap();
@@ -8,12 +27,12 @@ fn main()  {
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
 		input = input.trim().to_string();
-		match input.trim().split(" ").nth(0).unwrap() {
-			"exit" => {return ;},
-			"echo" => {println!("{}", input.split_off(5).trim())}
-			_ => {
-				println!("{}: command not found", input.trim());
+		if builtins.contains_key(input.split(" ").nth(0).unwrap()){
+			if builtins[&input](input) == 1 {
+				return;
 			}
+		}else{
+			println!("{}: command not found", input.trim());
 		}
     }
 }
