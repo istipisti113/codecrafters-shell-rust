1a6d2f9d4a01addd8b7a8c6e3991444d4042827a
Szabó István <pisti4395@gmail.com>
2025-03-17 21:05:01 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,10 @@ fn main()  {
         let stdin = io::stdin();
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
+		input = input.trim().to_string();
 		match input.trim().split(" ").nth(0).unwrap() {
 			"exit" => {return ;},
-			"echo" => {println!("{}", input.split_off(5))}
+			"echo" => {println!("{}", input.split_off(5).trim())}
 			_ => {
 				println!("{}: command not found", input.trim());
 			}
