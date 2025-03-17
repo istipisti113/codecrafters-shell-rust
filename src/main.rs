8935f71095dce71202db84f290ae146ec6d23f0a
Szabó István <pisti4395@gmail.com>
2025-03-17 20:54:41 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,17 @@
-#[allow(unused_imports)]
 use std::io::{self, Write};
 
-fn main() {
-    // Uncomment this block to pass the first stage
+fn main()  {
     loop{
         print!("$ ");
         io::stdout().flush().unwrap();
-        //
-        // Wait for user input
         let stdin = io::stdin();
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
-        run_command(input);
+		match input.trim() {
+			"exit 0" => {return ;},
+			_ => {
+				println!("{}: command not found", input.trim());
+			}
+		}
     }
-}
-
-fn run_command(command:String) {
-    println!("{}: command not found", command.trim());
 }
